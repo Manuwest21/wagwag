7a758bb2e0b8eab7bdc5537b3c8be6b4ae0699f8
Emmanuel DEVRIES <hammers178@hotmail.com>
2022-10-10 15:05:36 +0200

    first commit

---
+++ coinplon/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-+kz-fj_9f51m4t54r5w!j&4ff+88k@k^@(e_-rpv($042$c%3_"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
